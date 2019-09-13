92e29a4b8daff0bbaebfc31757d1885ed060e378
Andreas Malleus <andreasmalleus@gmail.com>
2019-09-13 09:32:25 +0300

    uus

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ app.get('/', (req, res) => {
 
 app.use(express.static('static'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`Our app is running on port ${ port }`);
+});
