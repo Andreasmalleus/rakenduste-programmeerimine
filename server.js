7dfdd8d87fd2cc452f1caa1780e4bb53c7cd2237
Andreas Malleus <andreasmalleus@gmail.com>
2019-09-13 09:39:30 +0300

    uus

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require("path");
-const port = 4000
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "static", "index.html"));
