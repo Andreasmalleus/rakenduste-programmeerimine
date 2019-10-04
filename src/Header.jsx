4c51c1598961bb110f858279a3394929298c0dd3
Andreas Malleus <andreasmalleus@gmail.com>
2019-10-04 11:41:19 +0300

    icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { cartIcon, loginIcon } from "./icons";
 
+console.log(cartIcon, "-- this is cartICon");
 
 const Header = () => {
     return (
@@ -13,6 +15,9 @@ const Header = () => {
             <div className={"buttons"}>
                 <button className={"loginSignupButton"}>Login/Signup</button>
                 <button className={"cartButton"}>Cart</button>
+                <img className="CartIcon" src={cartIcon} width="100"></img> 
+                <img className="LoginIcon" src={loginIcon} width="100"></img> 
+
             </div>
         </div>
     );
