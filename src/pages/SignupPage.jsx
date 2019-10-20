ac5f6daedf71cfe8259e773ec77d768832a01ca9
Andreasmalleus <andreasmalleus@gmail.com>
2019-10-20 14:39:30 +0300

    f

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -33,7 +33,7 @@ class SignupPage extends React.PureComponent{
                 "Content-Type" : "application/json"
             },
             body : JSON.stringify(this.state),
-        })
+        });
     }
 
     render(){
