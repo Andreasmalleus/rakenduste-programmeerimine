f280e3682098b58c01953a5896264edda8825916
Andreasmalleus <andreasmalleus@gmail.com>
2019-11-22 00:39:11 +0200

    casefix

---
+++ src/Index.jsx
@@ -0,0 +1,24 @@
+import ReactDom from "react-dom";
+import React from "react";
+import App from "./components/App.jsx";
+
+import "typeface-roboto";
+
+import {Provider} from "react-redux";
+import store from "./store/store.js";
+
+const root = document.getElementById("app");
+
+
+ReactDom.render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    root
+);
+
+
+
+
+
+
